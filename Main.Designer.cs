d0886afb7a53b14874d8b79853557bf7e4693412
Jonathan Devine <Jonathan.Devine@mhr.co.uk>
2022-10-31 15:29:54 +0000

    start in centre

--- Main.Designer.cs
+++ Main.Designer.cs
@@ -73,6 +73,7 @@ namespace SimpleTimer
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.ResumeLayout(false);
             this.PerformLayout();
